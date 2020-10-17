f7b1f82208b8b2006c37f79c403dc943281ba545
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-10-17 20:24:37 +0300

    create factory

---
+++ creational/factory.js
@@ -0,0 +1,40 @@
+class SimpleMembership {
+	constructor(name) {
+		this.name = name;
+		this.cost = 50;
+	}
+}
+
+class PremiumMembership {
+	constructor(name) {
+		this.name = name;
+		this.cost = 150;
+	}
+}
+
+class MemberFactory {
+	static list = {
+		simple: SimpleMembership,
+		premium: PremiumMembership
+	}
+
+	create(name, type = 'simple') {
+		const Membership = MemberFactory.list[type] || MemberFactory.list.simple;
+		const member = new Membership(name);
+		member.type = type;
+		member.define = function () {
+			console.log(`${ this.name } (${ this.type }): ${ this.cost }`)
+		}
+
+		return member;
+	}
+}
+
+const factory = new MemberFactory();
+
+const members = [
+	factory.create('Elise', 'simple'),
+	factory.create('Diana', 'premium')
+]
+
+members.forEach(m => m.define())
