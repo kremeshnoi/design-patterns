9e4c375086ffbba6b03cac036ef347f53c022f61
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-10-17 21:36:23 +0300

    create singleton

---
+++ creational/singleton.js
@@ -0,0 +1,25 @@
+// It is used when we need an object in a SINGLE INSTANCE !!!
+
+class Database {
+	constructor(data) {
+		/*
+			The key part.
+			Checking if the database is running to prevent "restarting" etc. (creating a new instance)
+		*/
+		if (Database.exist) return Database.instance;
+		// and only then
+		Database.instance = this;
+		Database.exist = true;
+		this.data = data;
+	}
+
+	getData() {
+		return this.data;
+	}
+}
+
+const mongo = new Database('MongoDB');
+console.log(mongo.getData());
+
+const mysql = new Database('MySQL');
+console.log(mysql.getData());
