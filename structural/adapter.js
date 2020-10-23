e28bc51e7028d71c6e643cc47167cb89a4f67cb0
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-10-23 04:30:47 +0300

    create adapter

---
+++ structural/adapter.js
@@ -0,0 +1,49 @@
+/*
+
+	This pattern is suitable for refactoring
+	and adding new functionality.
+
+*/
+
+class OldCalc {
+	operations(t1, t2, operation) {
+		switch (operation) {
+			case 'add': return t1 + t2;
+			case 'sub': return t1 - t2;
+			default: return NaN;
+		}
+	}
+}
+
+class NewCalc {
+	add(t1, t2) {
+		return t1 + t2;
+	}
+
+	sub(t1, t2) {
+		return t1 - t2;
+	}
+}
+
+class CalcAdapter {
+	constructor() {
+		this.calc = new NewCalc();
+	}
+
+	operations(t1, t2, operation) {
+		switch (operation) {
+			case 'add': return this.calc.add(t1, t2);
+			case 'sub': return this.calc.sub(t1, t2);
+			default: return NaN;
+		}
+	}
+}
+
+const oldCalc = new OldCalc();
+console.log(oldCalc.operations(10, 30, 'add'));
+
+const newCalc = new NewCalc();
+console.log(newCalc.add(40, 5));
+
+const calcAdapter = new CalcAdapter();
+console.log(calcAdapter.operations(25,10, 'sub'));
