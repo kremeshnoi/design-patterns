531b87f0fca1372f06eb42c3c78e2235d95686c2
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2021-09-05 01:32:04 +0300

    update singleton

--- singleton.js
+++ singleton.js
@@ -2,7 +2,7 @@
 
 	Classically, the Singleton pattern can be implemented
 	by creating a class with a method that creates a new instance
-	of the class if one doesn"t exist. In the event of an instance
+	of the class if one doesn't exist. In the event of an instance
 	already existing, it simply returns a reference to that object.
 
  */
