80a8527e054a9b8b796d6747567aaa4e6c4e1cfd
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2021-09-05 04:55:43 +0300

    update factory

--- factory.js
+++ factory.js
@@ -3,7 +3,7 @@
 	The Factory pattern is another creational pattern
 	concerned with the notion of creating objects.
 	Where it differs from the other patterns in its category
-	is that it doesn"t explicitly require us to use a constructor.
+	is that it doesn't explicitly require us to use a constructor.
 	Instead, a Factory can provide a generic interface for creating objects,
 	where we can specify the type of factory object we wish to be created.
 
